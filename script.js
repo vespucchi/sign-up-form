a0c6377dcbc3ce7da6b883ac11b7f02b8c66bd25
vespucchi <56511717+vespucchi@users.noreply.github.com>
2024-05-12 21:40:33 +0200

    Improve script to update password requirements style in real time

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-let pw = document.querySelector('#password');
+let pw = document.getElementById('password');
 let cpw = document.querySelector('#confpassword');
 let rpwInfo = document.querySelector('#pw-match');
+let upper = document.getElementById("pw-upper");
+let number = document.getElementById("pw-numb");
+let lower = document.getElementById("pw-lower");
+let char = document.getElementById("pw-chars");
+let list = document.querySelector(".pw-req");
 
 cpw.addEventListener("input", () => {
     if(pw.value !== cpw.value) {
@@ -9,3 +14,38 @@ cpw.addEventListener("input", () => {
         rpwInfo.classList.replace('visible', 'invisible');
     }
 });
+
+pw.addEventListener("input", () => {
+    // check for number
+    if(pw.value.match(/(?=.*\d)/) !== null) {
+        number.style.color = "green";
+    } else if((pw.value.match(/(?=.*\d)/) === null)) {
+        number.style.color = "red";
+    }
+
+    // check for lowercase
+    if(pw.value.match(/(?=.*[a-z])/) !== null) {
+        lower.style.color = "green";
+    } else if((pw.value.match(/(?=.*\d)/) === null)) {
+        lower.style.color = "red";
+    }
+
+    // check for uppercase
+    if(pw.value.match(/(?=.*[A-Z])/) !== null) {
+        upper.style.color = "green";
+    } else if((pw.value.match(/(?=.*\d)/) === null)) {
+        upper.style.color = "red";
+    }
+
+    // check for 8 chars
+    if(pw.value.length >= 8) {
+        char.style.color = "green";
+    } else if(pw.value.length < 8) {
+        char.style.color = "red";
+    }
+})
+
+
+function reload() {
+    location.reload();
+  }
